c1505842e80f862d04b7bcc5271512ecf0f0b089
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-15 16:30:07 +0530

    Create date.c

---
+++ date.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int d,m,y;
+  printf("enter date: ");
+  scanf("%d",&d);
+  printf("enter month: ");
+    scanf("%d",&m);
+  printf("enter year: ");
+    scanf("%d",&y);
+  printf(" %d / %d / %d ",d,m,y);
+  printf("\n");
+  printf(" Day-%d ,Month-%d ,Year-%d",d,m,y);
+
+
+}
