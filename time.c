4bf53ce29ee3ddac74a8013f3d615c7f8c289329
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-15 16:53:05 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int HH,MM;
+  printf("enter time: ");
+  scanf("%d",&HH);
+  printf("Enter Miniuts: ");
+  scanf("%d",&MM);
+  printf("%d : %d",HH,MM);
+  printf("\n");
+  printf("%d Hour and %d minute",HH,MM);
+}
