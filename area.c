6ce6c4b4d23b6ab3b741f9d3fc5246273d2d81bf
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-15 11:51:50 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int r;
+  float area;
+  printf("Enter A Redius");
+  scanf("%d",&r);
+  area = 3.14*r*r;
+  printf("Area of circle is %f Having Redius %d",area,r);
+}
